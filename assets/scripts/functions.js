cd143d74f609a06ebad437564350e8f8ad4b1ddc
experiment01 <mathex160@gmail.com>
2021-12-31 02:49:59 -0600

    0.3.3
    - Cleaning up project infoboxes
    - Making better use of classes
    - New content header
    - Cleaned up a few inconsistencies

--- assets/scripts/functions.js
+++ assets/scripts/functions.js
@@ -6,31 +6,48 @@ var expanded = false
 
 function reveal() {
     var arrow = document.getElementById("content-infobox-cloaks-banner-arrow-image")
+    var logo = document.getElementById("content-infobox-cloaks-banner-text-title-image")
     var description = document.getElementById("content-infobox-cloaks-banner-text-description")
-    var summary = document.getElementById("content-infobox-cloaks-summary")
+    var summary = document.getElementById("content-infobox-cloaks-expanded-summary")
     var box = document.getElementById("content-infobox-cloaks")
+    var links = document.getElementById("content-infobox-cloaks-expanded-links")
     if (expanded == true) {
         arrow.style.animation = "rotateNeg90 0.5s";
+        description.style.animation = "opacity100 0.5s"
         summary.style.animation = "opacity0 0.5s"
         box.style.animation = "height100 0.5s"
+        logo.style.animation = "rotateNeg360 0.5s"
+        arrow.parentElement.style.cursor = "auto"
+        arrow.parentElement.onclick = ""
+        summary.hidden = true
+        links.hidden = true
         setTimeout(function() {
             arrow.style.transform = "translateY(50%) rotate(-90deg)";
-            description.hidden = false
-            summary.hidden = true
+            arrow.parentElement.style.cursor = "pointer"
             box.style.height = "100px"
+            description.hidden = false
+            arrow.parentElement.onclick = reveal
+            logo.style.animation = ""
         }, 500)
         expanded = false
     }
     else {
         arrow.style.animation = "rotate0 0.5s";
-        summary.style.animation = "opacity100 0.5s"
-        box.style.animation = "height500 0.5s"
+        summary.style.animation = "opacity100 0.25s"
+        box.style.animation = "height230 0.5s"
+        logo.style.animation = "rotate360 0.5s"
+        description.hidden = true
+        arrow.parentElement.style.cursor = "auto"
+        arrow.parentElement.onclick = ""
         setTimeout(function() {
             arrow.style.transform = "translateY(50%) rotate(0deg)";
+            arrow.parentElement.style.cursor = "pointer"
             arrow.style.right = "11px"
-            description.hidden = true
+            box.style.height = "230px"
             summary.hidden = false
-            box.style.height = "500px"
+            links.hidden = false
+            arrow.parentElement.onclick = reveal
+            logo.style.animation = ""
         }, 500)
         expanded = true
     }
